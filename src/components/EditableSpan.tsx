73883a85873e6425bd75f96c589858f21000dddf
AndriiPrysiazhniuk <sarutora81@gmail.com>
2023-09-14 11:18:29 +0300

    Removed App.test.ts and added activateViewModeOnClick function to EditableSpan.tsx.

--- src/components/EditableSpan.tsx
+++ src/components/EditableSpan.tsx
@@ -1,6 +1,5 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, KeyboardEvent, useState} from "react";
 import {TextField} from "@mui/material";
-
 
 
 type EditableSpanPropsType = {
@@ -22,8 +21,15 @@ export const EditableSpan = (props: EditableSpanPropsType) => {
         setEditMode(false)
         onChange(title)
     }
+    const activateViewModeOnClick = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'Enter') {
+            setEditMode(false)
+            onChange(title)
+        }
+    }
     return editMode
-        ? <TextField type="text" onChange={onChangeInputHandler} value={title} onBlur={activateViewMode}
+        ? <TextField type="text" onChange={onChangeInputHandler} value={title} onKeyDown={activateViewModeOnClick}
+                     onBlur={activateViewMode}
                      autoFocus/>
         : <span onDoubleClick={setEditModeHandler}>{value}</span>
 }
